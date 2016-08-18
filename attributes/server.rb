2f06feeb70377589e49a372a7750a688ce2be39f
Dmitry Shmelev <avikez@gmail.com>
2016-08-18 18:02:47 +0300

    Add runit attributes

--- attributes/server.rb
+++ attributes/server.rb
@@ -4,5 +4,7 @@ default['cfssl']['server']['ca'] = '/etc/cfssl/ca.pem'
 default['cfssl']['server']['ca-key'] = '/etc/cfssl/ca-key.pem'
 default['cfssl']['server']['config-file'] = '/etc/cfssl/config.json'
 default['cfssl']['server']['config-directory'] = '/etc/cfssl/'
+default['cfssl']['server']['runit']['service'] = 'cfssl'
+default['cfssl']['server']['runit']['cookbook'] = 'cfssl'
 
 default['cfssl']['server']['csr'] = nil
