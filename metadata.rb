578e707970eb21f0dde39d1d6a90bbd3281b88cc
Dmitry Shmelev <avikez@gmail.com>
2016-08-19 16:37:12 +0400

    Add runit attributes (#7)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'operations@onetwotrip.com'
 license          'LGPLv3'
 description      'Installs/Configures cfssl'
 long_description 'Installs/Configures cfssl'
-version          '0.2.5'
+version          '0.2.6'
 
 issues_url       'https://github.com/onetwotrip/chef-cfssl/issues'
 source_url       'https://github.com/onetwotrip/chef-cfssl'
